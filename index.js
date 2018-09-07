6d15440a96c9c312612c8f64a04f4c7e426e3551
Amit Khonde <amit@hullo.ai>
2018-09-07 21:15:25 +0530

    Completed javascript part

---
+++ index.js
@@ -0,0 +1,14 @@
+let slides = document.getElementsByClassName('slide');
+let currentSlide = 0;
+window.setInterval(() => {
+  if(currentSlide == slides.length - 1) {
+    // reset all the classes and make currentSlide = 0;
+    for(var i = 0; i < slides.length; i++) {
+      slides[i].classList.remove("current-slide");
+    }
+    currentSlide = 0;
+  } else {
+      slides[currentSlide].classList.add("current-slide");
+      currentSlide++;
+  }
+}, 2000);
