66a36e43a111a1abe4b99e3302812b5370febc8c
Amit Khonde <amit@hullo.ai>
2018-09-07 21:41:54 +0530

    Changed css and added heading paragraphs

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let slides = document.getElementsByClassName('slide');
 let currentSlide = 0;
 window.setInterval(() => {
-  if(currentSlide == slides.length - 1) {
-    // reset all the classes and make currentSlide = 0;
-    for(var i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("current-slide");
-    }
-    currentSlide = 0;
-  } else {
-      slides[currentSlide].classList.add("current-slide");
-      currentSlide++;
-  }
+	if(currentSlide == slides.length - 1) {
+		// reset all the classes and make currentSlide = 0;
+		for(var i = 0; i < slides.length; i++) {
+			slides[i].classList.remove("current-slide");
+		}
+		
+		currentSlide = 0;
+	} else {
+		slides[currentSlide].classList.add("current-slide");
+		currentSlide++;
+	}
 }, 2000);
